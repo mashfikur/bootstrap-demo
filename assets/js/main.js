d22a325d9acebfa781a32105bc326f2bae734226
mashfikur <120697837+mashfikur@users.noreply.github.com>
2024-01-02 22:56:38 +0600

    raw css and js animation explored

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+let btn = document.getElementById("open-btn");
+let menu = document.getElementById("dropmenu");
+let close_it = true;
+
+btn.addEventListener("click", () => {
+  close_it = !close_it;
+  menu.classList.add("no-margin");
+
+  if (close_it) {
+    menu.classList.remove("no-margin");
+  }
+});
